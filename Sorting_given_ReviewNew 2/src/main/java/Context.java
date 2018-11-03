2023ccd67b2e9a42f3ba70c70b247e40923e7925
Adam Farmelo <afarmelo@asu.edu>
2018-11-03 17:01:40 -0400

    Finished Context class

--- Sorting_given_ReviewNew 2/src/main/java/Context.java
+++ Sorting_given_ReviewNew 2/src/main/java/Context.java
@@ -1,9 +1,28 @@
 package main.java;
-
+/**
+ * Context.java
+ * @author Adam Farmelo
+ * @version 11/3/2018
+ * 
+ * The Context class is the user side of the Strategy pattern. Through
+ * the context class you can acess any sort method you want, with only 
+ * an integer choice.
+ *
+ */
 import java.util.HashMap;
 
+
+/**
+ * Context provides a collection of SortMethods, and a map of strings
+ * for the end user to determine which sort method he'd like to use
+ * without any ugly switch statements or logical conditions.
+ */
 public class Context {
 
+	/**
+	 * Create a context object
+	 * @param choice a choice of sort method
+	 */
 	public Context(int choice) {
 		init();
 		
@@ -12,10 +31,19 @@ public class Context {
 		this.sorter = sorters[choice];
 	}
 	
+	/**
+	 * Call the sort method that is encapsulated in this class
+	 * which has been previously chosen on object creation
+	 * @param vec the vector to sort
+	 */
 	public void executeSort(Item[] vec) {
 		sorter.sort(vec);
 	}
 	
+	/**
+	 * Get your choice of sorting algorithm as a string
+	 * @return the string representation of the sort algorithm
+	 */
 	public String getSortChoice() {
 		if (choice == -1) {
 			throw new InvalidOperationException("No sorter chosen");
@@ -24,12 +52,12 @@ public class Context {
 	}
 	
 	private void init() {
-        choiceMap.put(0, "BubbleSort");
+        choiceMap.put(0, "Bubble Sort");
         choiceMap.put(1, "Selection Sort");
         choiceMap.put(2, "Insertion Sort");
         choiceMap.put(3, "Merge Sort");
         choiceMap.put(4, "Quick Sort");
-        choiceMap.put(5, "Bubble Sort");  
+        choiceMap.put(5, "Heap Sort");  
         
         sorters = new SortMethod[6];
         sorters[0] = new BubbleSort();
@@ -37,7 +65,7 @@ public class Context {
         sorters[2] = new InsertionSort();
         sorters[3] = new MergeSort();
         sorters[4] = new QuickSort();
-        sorters[5] = new BubbleSort();
+        sorters[5] = new HeapSort();
 	}
 	
 	private SortMethod sorter;
