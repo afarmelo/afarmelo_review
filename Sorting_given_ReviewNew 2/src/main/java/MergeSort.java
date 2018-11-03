cbe7d2b7fd34c00dcd22159cdb3e05a3b953ced0
Adam Farmelo <afarmelo@asu.edu>
2018-11-03 16:37:32 -0400

    Finished MergeSort

--- Sorting_given_ReviewNew 2/src/main/java/MergeSort.java
+++ Sorting_given_ReviewNew 2/src/main/java/MergeSort.java
@@ -16,7 +16,7 @@ public class MergeSort implements SortMethod {
     /**
      * Sorts with merge sort algorithm
      * @param vec the array to be sorted
-     * @exception NullPointerException if <code>vec</code> 
+     * @throws NullPointerException if <code>vec</code> 
      * is not initialized
      */
 	@Override
@@ -34,12 +34,14 @@ public class MergeSort implements SortMethod {
      * @param last end of the range
      */
     private static void mergeSort(Item[] vec, int first, int last) {
-            if (first == last) return;	
-            // divide vec into 2 equal parts
-            int middle = (first + last) / 2; 
-            mergeSort(vec, first, middle);   // sort the first part
-            mergeSort(vec, middle+1, last);  // sort the second part
-            merge(vec, first, middle, last); // merge the 2 sorted parts
+        if (first == last) {
+        	return;	
+        }
+        // divide vec into 2 equal parts
+        int middle = (first + last) / 2; 
+        mergeSort(vec, first, middle);   // sort the first part
+        mergeSort(vec, middle+1, last);  // sort the second part
+        merge(vec, first, middle, last); // merge the 2 sorted parts
     }
     
 	/**
@@ -50,37 +52,41 @@ public class MergeSort implements SortMethod {
      * @param right end of the second range
      */
     private static void merge(Item[] vec, int left, int middle, int right) {
-            int i, j;
-            int m = middle - left + 1; // length of first array region
-            int n = right - middle;    // length of second array region
-        
-            // make copies of array regions to be merged 
-            // (only the references to the items)
-            Item[] copy1 = new Item[m];
-            Item[] copy2 = new Item[n];
-            for (i = 0; i < m; i++) copy1[i] = vec[left + i]; 
-            for (j = 0; j < n; j++) copy2[j] = vec[middle + 1 + j]; 
-                
-            i = 0; j = 0;   
-            // merge copy1 and copy2 into  vec[left...right]
-            while (i < m && j < n) {
-                    if (copy1[i].key < copy2[j].key) {
-                            vec[left+i+j] = copy1[i];
-                            i++;
-                    } else {
-                            vec[left+i+j] = copy2[j];
-                            j++;
-                    }//endif
-            }//endwhile
-            if (j == n) { // second array region is completely handled, 
-                          // so copy rest of first region
-                    while (i < m) {
-                            vec[left+i+j] = copy1[i];
-                            i++;
-                    }
+        int i, j;
+        int m = middle - left + 1; // length of first array region
+        int n = right - middle;    // length of second array region
+    
+        // make copies of array regions to be merged 
+        // (only the references to the items)
+        Item[] copy1 = new Item[m];
+        Item[] copy2 = new Item[n];
+        for (i = 0; i < m; i++) {
+        	copy1[i] = vec[left + i]; 
+        }
+        for (j = 0; j < n; j++) {
+        	copy2[j] = vec[middle + 1 + j]; 
+        }
+            
+        i = 0; j = 0;   
+        // merge copy1 and copy2 into  vec[left...right]
+        while (i < m && j < n) {
+            if (copy1[i].key < copy2[j].key) {
+                    vec[left+i+j] = copy1[i];
+                    i++;
+            } else {
+                    vec[left+i+j] = copy2[j];
+                    j++;
+            }//endif
+        }//endwhile
+        if (j == n) { // second array region is completely handled, 
+            // so copy rest of first region
+            while (i < m) {
+                vec[left+i+j] = copy1[i];
+                i++;
             }
-            // if (i == m) do nothing, 
-            // rest of second region is already in place
+        }
+        // if (i == m) do nothing, 
+        // rest of second region is already in place
     }
 
 }
