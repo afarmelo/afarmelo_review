c89a752e5312bef33e481d8998de21a91b8d25e2
Adam Farmelo <afarmelo@asu.edu>
2018-11-03 16:12:37 -0400

    Added comment banners and information

--- Sorting_given_ReviewNew 2/src/main/java/SortMethod.java
+++ Sorting_given_ReviewNew 2/src/main/java/SortMethod.java
@@ -1,5 +1,34 @@
 package main.java;
 
-public interface SortMethod {
+/**
+ * SortMethod.java
+ * @author Adam Farmelo
+ * @version 11/3/2018
+ * 
+ * The sortMethod interface is the Strategy interface that all sorting 
+ * methods will override. The inspiration from this interface comes from 
+ * the Strategy design pattern. 
+ * 
+ * Link: https://www.tutorialspoint.com/design_pattern/strategy_pattern.htm
+ *
+ */
 
+/**
+ * SortMethod provides the method sort(Item[] vec) which must be
+ * overriden in any implementing classes. The abstraction provided
+ * here allows the user to run any sort method they want at run-time, 
+ * without the ugliness of a switch statement.
+ */
+public interface SortMethod {
+	
+	/*
+	 * The sort method is an abstract method which will enable the 
+	 * implementing methods to be called independently and gives flexibility
+	 * at program runtime. 
+	 * 
+	 * @param vec an Item array to be sorted
+	 */
+	public static void sort(Item[] vec) {
+		// sort functions override this method
+	};
 }
